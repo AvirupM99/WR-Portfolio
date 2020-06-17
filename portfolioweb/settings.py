c80a537d198d3c2fbbade54a0ad4d4e91a15990f
Avirup Maitra <wallstreetallblack99@gmail.com>
2020-06-17 10:11:28 +0000

    Remove old stuff

--- portfolioweb/settings.py
+++ portfolioweb/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'gamt!_v1$bo*u@y7zuv@q4ph^#dug49scw!715t&p#mbl&sqa)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rickpie.pythonanywhere.com']
 
 
 # Application definition
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
+STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
